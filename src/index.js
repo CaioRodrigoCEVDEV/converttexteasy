c97f3faf39745043b18dd3e2664f7c6f57a137a4
Caio Rodrigo <contato@caiorodrigocev.com.br>
2026-04-18 09:59:21 -0300

    Feat: Adicionado rota /sitemap para indexar no GOOGLE

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from "express"; //importar o express para criar o servidor web
+import path from "path"; //importar o path para lidar com caminhos de arquivos
+import { fileURLToPath } from "url"; //importar o fileURLToPath para obter o caminho do diretório atual
 
 //importar porta do arquivo .env
-import dotenv from "dotenv";
-dotenv.config();    
+import dotenv from "dotenv"; //importar o dotenv para carregar as variáveis de ambiente do arquivo .env
+dotenv.config();    //carregar as variáveis de ambiente do arquivo .env para process.env
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); //obter o caminho do diretório atual
 
-const app = express();
-app.use(express.json());
+const app = express(); //criar uma instância do express para configurar o servidor
+app.use(express.json()); //configurar o express para lidar com requisições JSON
 
 //servir arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, "..", "public")));
@@ -50,6 +50,12 @@ app.get("/reverse-text", (req, res) => {
 app.get("/strikethrough-text", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public/tools", "strikethrough-text.html"));
 });
+//rota para sitemap.xml
+app.get("/sitemap.xml", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "sitemap.xml"));
+});
+
+
 // redirecionar para a página inicial se a rota não for encontrada
 app.use((req, res) => {
   res.redirect("/");
